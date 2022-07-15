1939d4d438c3036a6c8946f62f83e7e40fd17380
SIG Service : BENTABET Ismail <ismail.bentabet@sigservice-dz.com>
2022-07-16 00:29:41 +0100

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,76 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Button,
+  FlatList,
+} from "react-native";
 
 export default function App() {
+  const [text, onChangeText] = React.useState("Useless Text");
+  const [todos, setTodos] = useState([]);
+
+  const addTodo = () => {
+    setTodos([...todos, text]);
+    onChangeText("");
+  };
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.nav}>
+        <Text style={styles.heading}>TODO LIST</Text>
+      </View>
+      <View>
+        <FlatList data={todos} renderItem={({ item }) => <Text>{item}</Text>} />
+      </View>
+      <View style={styles.formContainer}>
+        <TextInput
+          style={styles.input}
+          onChangeText={onChangeText}
+          value={text}
+        />
+        <Button title="Add Task " color="#841584" onPress={addTodo} />
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  formContainer: {
+    flexDirection: "row",
+    flexWrap: "wrap",
+  },
+  nav: {
+    backgroundColor: "#000",
+
+    justifyContent: "center",
+    alignItems: "center",
+    borderBottomWidth: 1,
+    borderBottomColor: "#ccc",
+  },
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+  },
+  heading: {
+    color: "#fff",
+    fontSize: 20,
+    fontWeight: "600",
+    marginTop: 30,
+    marginBottom: 10,
+    marginLeft: 20,
+  },
+  scrollView: {
+    marginBottom: 70,
+  },
+  taskContainer: {
+    marginTop: 20,
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
   },
 });
